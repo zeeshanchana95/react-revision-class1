2650f19b51d5896a881b08d787e41722073e39e5
Zeeshan Ahmed <zeeshanchana95@gmail.com>
2023-02-18 16:58:48 +0500

    applied css

--- src/SampleCard.jsx
+++ src/SampleCard.jsx
@@ -8,8 +8,8 @@ function SampleCard(props) {
         <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://charmouth.org/charmouth_village/wp-content/uploads/2016/07/SE3.jpg" />
       <Card.Body>
-        <Card.Title>{props.firstName}</Card.Title>
-        <Card.Text>
+        <Card.Title style={{ color: 'blue' }}>{props.firstName}</Card.Title>
+        <Card.Text style={{ color: 'blue' }}>
           {props.lastName}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
